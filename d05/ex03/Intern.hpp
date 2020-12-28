306f1e7eeaa1fa5bbaa28bd5f1394c6ef6f6f9c9
Zouheir Layine <zlayine@e2r5p5.1337.ma>
2020-12-28 15:43:58 +0100

    d05 ex03

--- d05/ex03/Intern.hpp
+++ d05/ex03/Intern.hpp
@@ -16,7 +16,7 @@ public:
 };
 
 
-typedef Form* (Form::*PTRS) (std::string const &);
+// typedef Form* (Form::*PTRS) (std::string const &);
 
 Intern::Intern()
 {
@@ -30,16 +30,16 @@ Form	*Intern::makeForm(std::string const & type, std::string const & target)
 {
 	std::string names[] = {"robotomy request", "presidential pardon", "shrubbery creation"};
 
-	PTRS		ptrs[3] = { new PresidentialPardonForm, new RobotomyRequestForm, new ShrubberyCreationForm};
+	// PTRS		ptrs[3] = { new PresidentialPardonForm, new RobotomyRequestForm, new ShrubberyCreationForm};
 
-    for(int i = 0; i < 3; i++)
-    {
-        if (names[i].compare(type) == 0)
-        {  
-        	(*ptrs[i])(target);
-            break ;
-        }
-    }
+    // for(int i = 0; i < 3; i++)
+    // {
+    //     if (names[i].compare(type) == 0)
+    //     {  
+    //     	(*ptrs[i])(target);
+    //         break ;
+    //     }
+    // }
 }
 
 
