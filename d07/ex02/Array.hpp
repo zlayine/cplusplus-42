ca8eb348b9639b2751b75965de6a6d293940199b
Zouheir Layine <zlayine@e2r5p5.1337.ma>
2021-01-06 15:15:21 +0100

    ex02 d07

---
+++ d07/ex02/Array.hpp
@@ -0,0 +1,85 @@
+#ifndef ARRAY_H
+# define ARRAY_H
+
+#include <iostream>
+
+template<class T>
+class Array{
+
+private:
+	T	*array;
+	int	len;
+
+public:
+	Array()
+	{
+		array = new T;
+		len = 0;
+	}
+
+	Array(unsigned int n)
+	{
+		array = new T[n];
+		len = n;
+		for(int i = 0; i < n; i++)
+		{
+			array[i] = 0;
+		}
+	}
+
+	Array(Array const &src)
+	{
+		array = new T[src.size()];
+		len = src.size();
+		for(int i = 0; i < len; i++)
+		{
+			array[i] = src.array[i];
+		}
+	}
+
+	~Array()
+	{
+		delete[] array;
+	}
+
+	Array&	operator=(Array const &rhs)
+	{
+		delete[] array;
+		array = new T[rhs.size()];
+		len = rhs.size();
+		for(int i = 0; i < len; i++)
+		{
+			array[i] = rhs.array[i];
+		}
+		return *this;
+	}
+
+	T&	operator[](int i)
+	{
+		std::cout << "iter called " << i << std::endl;
+		if (i >= len)
+		{
+			std::cout << "Index out of bounds" << std::endl; 
+			//exception to throw
+		}
+		std::cout << "iter called " << i << std::endl;
+		return array[i];
+	}
+
+	void* operator new(std::size_t s)
+    {
+        return ::operator new(s);
+    }
+
+	void* operator new[](std::size_t s)
+    {
+        return ::operator new(s);
+    }
+
+	int	size() const
+	{
+		return (len);
+	}
+};
+
+#endif
---
+++ d07/ex02/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Array.hpp"
+
+int main()
+{
+	Array<int> *a = new Array<int>[2];
+
+	// a[0] = 5;
+	// std::cout << *a << std::endl;
+
+	return (0);
+}
