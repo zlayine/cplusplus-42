47007dc7468613f2e528d9aa991bc90657e61f02
Zouheir Layine <zlayine@e2r5p5.1337.ma>
2021-01-23 11:20:24 +0100

    fix

--- d06/ex01/Data.cpp
+++ d06/ex01/Data.cpp
@@ -6,12 +6,12 @@ void	*serialize()
 	std::string randoms("abcdefghijklmnopqrstuvwxyz0123456789");
 
 	srand(clock());
-	data = new char[20];
+	data = new char[52];
 	for (int i = 0; i < 8; i++)
 		data[i] = randoms[rand() % 36];
-	*reinterpret_cast<int*>(data + 8) = rand() % 1000;
+	*reinterpret_cast<int*>(data + 24) = rand() % 1000;
 	for (int i = 0; i < 8; i++)
-		data[i + 12] = randoms[rand() % 36];
+		data[i + 28] = randoms[rand() % 36];
 	return data;
 }
 
@@ -20,7 +20,7 @@ Data	*deserialize(void *raw)
 	Data *d = new Data;
 
 	d->s1 = std::string(static_cast<char *>(raw), 8);
-	d->s2 = std::string(static_cast<char *>(raw) + 12, 8);
-	d->n = *reinterpret_cast<int*>(static_cast<char *>(raw) + 8);
+	d->s2 = std::string(static_cast<char *>(raw) + 28, 8);
+	d->n = *reinterpret_cast<int*>(static_cast<char *>(raw) + 24);
 	return d;
 }
